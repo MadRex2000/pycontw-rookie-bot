cb586745c3183a902e7e7bb4924bfc20ab401ca9
MadRex2000 <spplkk113@gmail.com>
2021-06-27 20:40:53 +0000

    feat(auto_remove_role): auto remove rookie_volunteer

--- src/main.py
+++ src/main.py
@@ -20,5 +20,11 @@ async def on_member_join(member):
     role = discord.utils.get(member.guild.roles, name='rookie_volunteer')
     await member.add_roles(role)
 
+@client.event
+async def on_member_update(before, after):
+    member = after
+    role = discord.utils.get(member.guild.roles, name='rookie_volunteer')
+    if before.roles != after.roles and role in member.roles and len(member.roles) > 2:
+        await member.remove_roles(role)
 
 client.run(TOKEN)
